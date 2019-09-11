f7274b37832f658e02cf7944cef82ea0efcd3518
Jafar Haq <jhaq@bsd220.org>
2019-09-11 10:28:39 -0500

    Initial Commit

---
+++ iOS App: Multiply/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS App: Multiply
+//
+//  Created by Jafar on 9/11/19.
+//  Copyright © 2019 Jafar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS App: MultiplyTests/iOS_App__MultiplyTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_App__MultiplyTests.swift
+//  iOS App: MultiplyTests
+//
+//  Created by Jafar on 9/11/19.
+//  Copyright © 2019 Jafar. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_App__Multiply
+
+class iOS_App__MultiplyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
