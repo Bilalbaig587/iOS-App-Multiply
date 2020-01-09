af1e1fd38ca09870f560ee74cb02ad77724ff59d
Bilal Baig <bbaig@bsd220.org>
2020-01-09 10:20:59 -0600

    save project

--- iOS App: Multiply/ViewController.swift
+++ iOS App: Multiply/ViewController.swift
@@ -9,12 +9,119 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstNumber: UITextField!
+    @IBOutlet weak var secondNumber: UITextField!
+    @IBOutlet weak var AnswerLabel: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func OnButton(_ sender: Any)
+    {
+        if var firstNumber = Int(firstNumber.text!){
+            if var secondNumber = Int(secondNumber.text!)
+            {
+                var answer = firstNumber * secondNumber
+                AnswerLabel.text = String(answer)
+                if answer == 64
+                {
+                    imageView.image = UIImage(named: "icecream")
+                    
+                }
+                else if answer % 2 == 0
+                {
+                    imageView.image = UIImage(named: "cake")
+                }
+                else
+                {
+                    imageView.image = UIImage(named: "milkshake")
+                }
+            }
+        }
+        view.endEditing(true)
+    }
+    
+    @IBAction func AddButton(_ sender: Any)
+    {
+        if var firstNumber = Int(firstNumber.text!){
+            if var secondNumber = Int(secondNumber.text!)
+            {
+                var answer = firstNumber + secondNumber
+                AnswerLabel.text = String(answer)
+                if answer == 64
+                {
+                    imageView.image = UIImage(named: "icecream")
+                    
+                }
+                else if answer % 2 == 0
+                {
+                    imageView.image = UIImage(named: "cake")
+                }
+                else
+                {
+                    imageView.image = UIImage(named: "milkshake")
+                }
+            }
+        }
+        view.endEditing(true)
+    }
+    
+    @IBAction func Subtract(_ sender: Any)
+    {
+        if var firstNumber = Int(firstNumber.text!){
+            if var secondNumber = Int(secondNumber.text!)
+            {
+                var answer = firstNumber - secondNumber
+                AnswerLabel.text = String(answer)
+                if answer == 64
+                {
+                    imageView.image = UIImage(named: "icecream")
+                    
+                }
+                else if answer % 2 == 0
+                {
+                    imageView.image = UIImage(named: "cake")
+                }
+                else
+                {
+                    imageView.image = UIImage(named: "milkshake")
+                }
+            }
+        }
+        view.endEditing(true)
+    }
+    
+    @IBAction func Divide(_ sender: Any)
+    {
+        if var firstNumber = Int(firstNumber.text!){
+            if var secondNumber = Int(secondNumber.text!)
+            {
+                var answer = firstNumber / secondNumber
+                AnswerLabel.text = String(answer)
+                if answer == 64
+                {
+                    imageView.image = UIImage(named: "icecream")
+                    
+                }
+                else if answer % 2 == 0
+                {
+                    imageView.image = UIImage(named: "cake")
+                }
+                else
+                {
+                    imageView.image = UIImage(named: "milkshake")
+                }
+            }
+        }
+        view.endEditing(true)
+    }
+    
+    
+    
+    
 }
 
